fef1b6db546190cba308bcc4272c0aee2e42cfd6
KaioCalas <kaiomonteiro151@gmail.com>
2022-11-18 10:31:35 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,6 +2,7 @@ import icon from '../../assets/img/icon-notification.svg'
 import axios from 'axios'
 import './styles.css'
 import { BASE_URL } from '../../utils/request';
+import { toast } from 'react-toastify';
 
 type Props = {
     saleId: number;
@@ -11,7 +12,7 @@ type Props = {
 function handleClick(id :number){
     axios(`${BASE_URL}/sales/${id}/notification`)
         .then(response => {
-            console.log('SUCESSO!');
+            toast.info("SMS enviado com sucesso!")
         })
 }
 
